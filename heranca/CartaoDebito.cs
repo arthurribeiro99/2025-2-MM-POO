08a7fc5327d02d13fc163c3f3d56d5e7406ed677
arthurribeiro99 <arthurribeiro7788@gmail.com>
2025-09-16 00:12:47 +0000

    feat: classe cartao

---
+++ heranca/CartaoDebito.cs
@@ -0,0 +1,23 @@
+using System.Data;
+
+public class CartaoDebito
+{
+    public CartaoDebito()
+    {
+        Bandeira = BandeiraCartao.Mastercard;
+    }
+
+    public string Numero { get; set; }
+    public BandeiraCartao Bandeira { get; set; }
+    public DateTime Vencimento { get; set; }
+    public string Portador { get; set; }
+    public string Cvv { get; set; }
+}
+
+public enum BandeiraCartao
+{
+    Visa,
+    Mastercard,
+    Amex,
+    Elo,
+}
